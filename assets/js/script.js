4e2b26151e1ee95f646ceb13571f880a31b15239
Lightfooted <vesselofbalance@hotmail.com>
2021-07-22 14:19:33 -0400

    playing with storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@
 var presentDay = moment().format('LLL');
 $("#currentDay").append(presentDay);
 //Don't see the difference between append and text
+
+//Save Button Click
+$(".saveBtn").on("click", function() {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //Store Area
+    localStorage.setItem(time, value);
+});
